1d6d7c89abc3f460dc27417b79fd2f09bfec9a82
Andrew-Ponomarenko <61511780+Andrew-Ponomarenko@users.noreply.github.com>
2023-05-23 10:01:07 +0300

    fixed models

--- gallery/models.py
+++ gallery/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+class Image(models.Model):
+    url = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/')
+    categories = models.ManyToManyField(Category,blank=True)
+    created_date = models.DateField()
+    age_limit = models.PositiveIntegerField()
